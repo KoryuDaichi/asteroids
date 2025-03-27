02e7f74164e790ac958e9e3b2bb395506e920f92
KoryuDaichi <mcginnis.cole@gmail.com>
2025-03-26 23:37:01 -0500

    add player.py to generate player sprite, add player radius to constants, set up main to draw the player sprite, update .gitignore

--- main.py
+++ main.py
@@ -3,24 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
-gameclock = pygame.time.Clock()
-dt = 0
+
 
 def main():
+    pygame.init()
+    gameclock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 == 1:
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
+
+
         pygame.display.flip()
         dt = gameclock.tick(60) / 1000
-        print(dt)
-        
+                
         
 if __name__ == "__main__":
     main()
