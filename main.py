795c8b700b5fff063567eca13177b97635980107
KoryuDaichi <mcginnis.cole@gmail.com>
2025-03-28 22:56:38 -0500

    Add player movement and add groups

--- main.py
+++ main.py
@@ -14,15 +14,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        updateable.update(dt)
 
         pygame.display.flip()
         dt = gameclock.tick(60) / 1000
